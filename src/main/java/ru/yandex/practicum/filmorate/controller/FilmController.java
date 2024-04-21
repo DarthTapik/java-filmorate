319ea642b1abe8a7f7774d0b5526156dc773be13
DarthTapik <darthtapik@yandex.ru>
2024-04-21 13:47:25 +0300

    Create Film and User Controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,55 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    private final Logger log = LoggerFactory.getLogger(FilmController.class);
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Фильм " + film.getId() + " создан");
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
+            log.info("Фильм " + film.getId() + " обновлен");
+            return film;
+        } else {
+            throw new NotFoundException("Фильм с id " + film.getId() + " не найден");
+        }
+    }
+
+
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,59 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import jakarta.validation.Valid;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Integer, User> users = new HashMap<>();
+
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        user.setId(getNextId());
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Пользователь " + user.getId() + " создан");
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info("Пользователь " + user.getId() + " обновлен");
+            return user;
+        } else {
+            throw new NotFoundException("Пользователь с id " + user.getId() + " не найден");
+        }
+    }
+
+
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
